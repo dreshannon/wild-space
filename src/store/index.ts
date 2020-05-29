db4c83b378882974900c438750ceee6560a5af5f
Dre Shannon <leandre.d.shannon@gmail.com>
2020-05-28 20:13:32 -0500

    Makes default character in vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,31 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {Character} from '../types';
 
 Vue.use(Vuex);
 
+const character: Character = {
+  name: '',
+  traits: {
+    strength: 0,
+    dexterity: 0,
+    relations: 0,
+    culture: 0,
+    biology: 0,
+    engineering: 0,
+  },
+  skills: [],
+  health: 0,
+  currentHealth: 0,
+  languages: [],
+  background: '',
+  personality: '',
+  picture: '',
+};
+
 export default new Vuex.Store({
   state: {
+    character,
   },
   mutations: {
   },
