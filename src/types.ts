1ac87c1f4385c6912784e3838efcb8b5ef5b4619
Dre Shannon <leandre.d.shannon@gmail.com>
2020-05-28 20:13:08 -0500

    Adds character type

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export interface Campaign {
+  name: string;
+}
+
+export interface Character {
+  name: string;
+  traits: {
+    strength: number;
+    dexterity: number;
+    relations: number;
+    culture: number;
+    biology: number;
+    engineering: number;
+  };
+  skills: Skill[];
+  health: number;
+  currentHealth: number;
+  languages: string[];
+  background: string;
+  personality: string;
+  picture: string;
+}
+
+export interface Skill {
+  name: string;
+  description: string;
+}
