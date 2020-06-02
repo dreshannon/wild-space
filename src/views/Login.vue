296d2c91439d19db1d359ef4756a9870f69e854b
Dre Shannon <leandre.d.shannon@gmail.com>
2020-06-02 10:40:53 -0500

    Creates login page markup

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,31 @@
 <template>
-  <div />
+  <div id="login">
+    <section>
+      <v-form @submit.prevent>
+        <v-card>
+          <v-card-title>
+            Welcome to the Wild Space Campaign Tracker!
+          </v-card-title>
+          <v-card-text>
+              <v-text-field
+                label="Email"
+                type="email"
+                v-model.trim="loginForm.email"
+              />
+              <v-text-field
+                label="Password"
+                type="password"
+                v-model.trim="loginForm.password"
+              />
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn>Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,6 +33,10 @@ import {Component, Vue} from 'vue-property-decorator';
 
 @Component
 export default class Login extends Vue {
+  loginForm = {
+    email: '',
+    password: '',
+  };
 }
 </script>
 
