106d2433768a4be9f303431a5d8bf430548c876d
Dre Shannon <leandre.d.shannon@gmail.com>
2020-06-02 09:01:03 -0500

    Sets up firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,40 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+// firebase init goes here
+const config = {
+  apiKey: 'AIzaSyD9Kqn6cU6MYschxMlG_vU56AqyqqbQ7Nc',
+  authDomain: 'wild-space.firebaseapp.com',
+  databaseURL: 'https://wild-space.firebaseio.com',
+  projectId: 'wild-space',
+  storageBucket: 'wild-space.appspot.com',
+  messagingSenderId: '885249845133',
+  appId: '1:885249845133:web:ca721ab6176b907badffc4',
+  measurementId: 'G-Y2MZX4SR5N',
+};
+firebase.initializeApp(config);
+
+// firebase utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+const currentUser = auth.currentUser;
+
+// date issue fix according to firebase
+const settings = {
+  timestampsInSnapshots: true,
+};
+db.settings(settings);
+
+// firebase collections
+const usersCollection = db.collection('users');
+const campaignsCollection = db.collection('campaigns');
+const charactersCollection = db.collection('characters');
+
+export default {
+  db,
+  auth,
+  currentUser,
+  usersCollection,
+  campaignsCollection,
+  charactersCollection,
+};
