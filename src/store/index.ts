a83f8a58178e6239e8f83c67ca121292c04c02ad
Dre Shannon <leandre.d.shannon@gmail.com>
2020-06-02 17:49:59 -0500

    Adds ability yo login and sign in and set current user

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,27 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import CookieService from '../services/cookie-service';
 import {Event} from '@/types';
+import fb from '../firebaseConfig';
 
 Vue.use(Vuex);
 
+const currentUser: any | null = null;
+
 export default new Vuex.Store({
   state: {
+    currentUser,
+    userProfile: {},
     campaign: CookieService.getCampaignCookie(),
     character: CookieService.getCharacterCookie(),
     notes: CookieService.getCampaignNotes(),
   },
   mutations: {
+    setCurrentUser(state, val) {
+      state.currentUser = val;
+    },
+    setUserProfile(state, val) {
+      state.userProfile = val;
+    },
     setCharacterInventory(state, inventory: string) {
       state.character.inventory = inventory;
     },
@@ -50,6 +61,15 @@ export default new Vuex.Store({
     },
   },
   actions: {
+    fetchUserProfile({commit, state}) {
+      fb.usersCollection.doc(state.currentUser.uid).get()
+        .then((res) => {
+          commit('setUserProfile', res.data());
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
   modules: {
   },
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,122 @@
 <template>
   <div id="login">
-    <section>
-      <v-form @submit.prevent>
-        <v-card>
-          <v-card-title>
-            Welcome to the Wild Space Campaign Tracker!
-          </v-card-title>
-          <v-card-text>
-              <v-text-field
-                label="Email"
-                type="email"
-                v-model.trim="loginForm.email"
-              />
-              <v-text-field
-                label="Password"
-                type="password"
-                v-model.trim="loginForm.password"
-              />
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn>Login</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-form>
-    </section>
+    <v-form
+      v-if="showLoginForm"
+      @submit.prevent="login()"
+    >
+      <v-card>
+        <v-card-title>
+          Welcome to the Wild Space Campaign Tracker!
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model.trim="loginForm.email"
+            label="Email"
+            type="email"
+          />
+          <v-text-field
+            v-model.trim="loginForm.password"
+            label="Password"
+            type="password"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <a @click="showLoginForm = false">
+            Create an Account
+          </a>
+          <v-spacer />
+          <v-btn
+            @click="login()"
+          >
+            Login
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+    <v-form
+      v-else
+    >
+      <v-card>
+        <v-card-title>Welcome to Wild Space Campaign Tracker!</v-card-title>
+        <v-card-subtitle>Please fill in the information below to get started.</v-card-subtitle>
+        <v-card-text>
+          <v-text-field
+            v-model.trim="signupForm.name"
+            label="Name"
+          />
+          <v-text-field
+            v-model.trim="signupForm.email"
+            type="email"
+            label="Email"
+          />
+          <v-text-field
+            v-model.trim="signupForm.passwaord"
+            type="password"
+            label="Password"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <a @click="showLoginForm = true">
+            Already have an account?
+          </a>
+          <v-spacer />
+          <v-btn
+            @click="signup()"
+          >
+            Signup
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
+import fb from '../firebaseConfig';
 
 @Component
 export default class Login extends Vue {
+  showLoginForm = true;
   loginForm = {
     email: '',
     password: '',
   };
+  signupForm = {
+    name: '',
+    email: '',
+    passwaord: '',
+  };
+
+  login() {
+    fb.auth.signInWithEmailAndPassword(this.loginForm.email, this.loginForm.password)
+      .then((user: any) => {
+        this.$store.commit('setCurrentUser', user.user);
+        this.$store.dispatch('fetchUserProfile');
+        this.$router.push('/home');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  signup() {
+    fb.auth.createUserWithEmailAndPassword(this.signupForm.email, this.signupForm.passwaord)
+      .then((user: any) => {
+        this.$store.commit('setCurrentUser', user);
+
+        fb.usersCollection.doc(user.uid).set({
+          name: this.signupForm.name,
+        })
+          .then(() => {
+            this.$store.dispatch('fetchUserProfile');
+            this.$router.push('/');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+  }
 }
 </script>
 
