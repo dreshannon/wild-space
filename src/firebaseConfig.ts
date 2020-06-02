074078ddafc1ebcf926d3308e2501ccd367919a8
Dre Shannon <leandre.d.shannon@gmail.com>
2020-06-02 10:20:05 -0500

    Removes unnessary config

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -19,12 +19,6 @@ const db = firebase.firestore();
 const auth = firebase.auth();
 const currentUser = auth.currentUser;
 
-// date issue fix according to firebase - NO LONGER NECESSARY
-// const settings = {
-//   timestampsInSnapshots: true,
-// };
-// db.settings(settings);
-
 // firebase collections
 const usersCollection = db.collection('users');
 const campaignsCollection = db.collection('campaigns');
