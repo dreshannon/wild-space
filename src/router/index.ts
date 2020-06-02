8068008adb34afad364b7b2a0531e65fb998c34b
Dre Shannon <leandre.d.shannon@gmail.com>
2020-06-02 08:59:40 -0500

    Lazy-loading edit character component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
 import Home from '../views/Home.vue';
-import EditCharacter from '../views/EditCharacter.vue';
 
 Vue.use(VueRouter);
 
@@ -14,15 +13,16 @@ const routes: Array<RouteConfig> = [
   {
     path: '/character',
     name: 'Character',
-    component: EditCharacter,
+    component: () => import(/* webpackChunkName: "character" */'../views/EditCharacter.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '*',
+    redirect: '/',
   },
 ];
 
