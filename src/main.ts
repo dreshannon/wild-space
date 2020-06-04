be29ab95931ad2b22e014ddfcd0e2649005c31a0
Dre Shannon <leandre.d.shannon@gmail.com>
2020-06-03 23:35:05 -0500

    Fixes reference for vuex store

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
-import store from './store';
+import {store} from './store';
 import 'es6-promise/auto';
 import fb from './firebaseConfig';
 
