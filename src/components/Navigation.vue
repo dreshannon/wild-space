0aaf89d874d710638fdf7a44f4ecbf6eacd275e1
Dre Shannon <leandre.d.shannon@gmail.com>
2020-06-11 14:36:21 -0500

    Removes account settings from navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,14 +11,6 @@
       class="mx-1"
       color="secondary"
       text
-      @click="showAccountModal = true"
-    >
-      <v-icon>mdi-account-circle</v-icon> Account
-    </v-btn>
-    <v-btn
-      class="mx-1"
-      color="secondary"
-      text
       @click="toggleSettings"
     >
       <v-icon>mdi-cog</v-icon> Settings
@@ -31,33 +23,6 @@
     >
       <v-icon>mdi-logout</v-icon> Logout
     </v-btn>
-    <v-dialog
-      v-model="showAccountModal"
-      max-width="290"
-    >
-      <v-card>
-        <v-card-title>Account</v-card-title>
-        <v-card-text>
-          <v-text-field
-            v-model="userProfile.name"
-            label="Name"
-          />
-          <v-select
-            v-model="userProfile.role"
-            label="Role"
-            :items="roles"
-          />
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            @click="saveUser()"
-          >
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-app-bar>
 </template>
 
@@ -76,23 +41,6 @@ export default class Navigation extends Vue {
   roles = ['Player', 'Game Master'];
   showAccountModal = false;
   showSettingsDrawer = false;
-
-  created() {
-    if (!this.userProfile.role) {
-      fb.usersCollection.doc(this.$store.state.currentUser.uid).set({
-        name: this.userProfile.name,
-        role: 'Player',
-      });
-    }
-  }
-
-  saveUser() {
-    fb.usersCollection.doc(this.$store.state.currentUser.uid).set({
-      name: this.userProfile.name,
-      role: this.userProfile.role,
-    });
-    this.showAccountModal = false;
-  }
 }
 </script>
 
