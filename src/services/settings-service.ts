db6d8c6726b292fa5ec2e9aed41327433ecb0eec
Dre Shannon <leandre.d.shannon@gmail.com>
2020-09-07 22:27:10 -0500

    Adds theme and prganization defaults and removes getters and setters from settings service

--- src/services/settings-service.ts
+++ src/services/settings-service.ts
@@ -1,8 +1,8 @@
 import CookieService from './cookie-service';
 import {customTheme} from '../theme';
 import cookieService from './cookie-service';
-import fb from '../firebaseConfig';
-import {User} from '@/types';
+import fb from '@/firebaseConfig';
+import {User, OrganizationSettings} from '@/types';
 
 function saveUser(uid: string, user: User) {
   fb.usersCollection.doc(uid).set({
@@ -11,61 +11,49 @@ function saveUser(uid: string, user: User) {
   });
 }
 
-function setOrganization(settings: any) {
-  CookieService.setOrganizationCookie(settings);
+function getDefaultOrganization() {
+  return {
+    campaignTracker: {
+      size: 4,
+      position: 1,
+    },
+    characterSheet: {
+      size: 8,
+      position: 2,
+    },
+    notes: {
+      size: 12,
+      position: 3,
+    },
+    characterNotes: {
+      size: 12,
+      position: 5,
+    },
+  };
 }
 
-function getOrganization() {
-  const settings = cookieService.getOrganizationCookie();
-  if (settings) {
-    return JSON.parse(settings);
-  } else {
-    return {
-      campaignTracker: {
-        size: '4',
-        position: '1',
-      },
-      characterSheet: {
-        size: '8',
-        position: '2',
-      },
-      notes: {
-        size: '12',
-        position: '3',
-      },
-      characterNotes: {
-        size: 12,
-        position: 5,
-      },
-    };
-  }
-}
 function resetOrganization() {
   CookieService.resetCookie('organization');
 }
 
-function setTheme(theme: any) {
-  CookieService.setThemeCookie(theme);
-}
-
-function getTheme() {
-  const theme = CookieService.getThemeCookie();
-  if (theme) {
-    return JSON.parse(theme);
-  }
-  return customTheme;
-}
-
-function resetTheme() {
-  CookieService.resetCookie('theme');
+function getDefaultTheme() {
+  return {
+    primary: '#210E39',
+    secondary: '#53208a',
+    background: '#FEFFFE',
+    headerText: '#FEFFFE',
+    bodyText: '#210E39',
+    accent: '#560D55',
+    error: '#960200',
+    info: '#8C8CF2',
+    success: '#DDABF8',
+    warning: '#A66102',
+  };
 }
 
 export default {
   saveUser,
-  setOrganization,
-  getOrganization,
+  getDefaultOrganization,
   resetOrganization,
-  setTheme,
-  getTheme,
-  resetTheme,
+  getDefaultTheme,
 };
